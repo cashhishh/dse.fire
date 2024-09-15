4a430c3a5f1d645ba64a3bcf143f63e287e84c7c
cashhishh <158817754+cashhishh@users.noreply.github.com>
2024-09-16 00:51:31 +0530

    Update AnimatedImages.jsx

--- src/Components/AnimatedImages.jsx
+++ src/Components/AnimatedImages.jsx
@@ -1,6 +1,6 @@
 // src/AnimatedImages.jsx
 import React from "react";
-import useIntersectionObserver from "./UseIntersectionObserver";
+import UseIntersectionObserver from "./UseIntersectionObserver";
 import "./AnimatedImages.css"; // Custom styles
 
 const images = [
